3c453a144fc3489e2930cece9a4b08cee6d16ef0
Damianos <dgiankakis@citycollege.sheffield.eu>
2023-04-26 19:09:27 +0300

    Continues on weather-search comp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'dg-ng-weather-app';
+  title = "dg-ng-weather-app";
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,42 +1,36 @@
+import { animate, state, style, transition, trigger } from "@angular/animations";
 import { Component } from "@angular/core";
-import { gsap } from "gsap";
 
 @Component({
   selector: "app-header",
   templateUrl: "./header.component.html",
   styleUrls: ["./header.component.scss"],
+  animations: [
+    trigger("openClose", [
+      state(
+        "open",
+        style({
+          opacity: 1,
+          left: 0,
+        })
+      ),
+      state(
+        "closed",
+        style({
+          opacity: 0.5,
+          left: "100%",
+        })
+      ),
+      transition("open => closed", [animate(".66s")]),
+      transition("closed => open", [animate(".33s")]),
+    ]),
+  ],
 })
 export class HeaderComponent {
   searchText = "";
-  timeline: gsap.core.Timeline;
+  isSearchOpen: boolean = false;
 
-  ngOnInit() {
-    this.timeline = gsap.timeline({ paused: true });
-    this.timeline.from(".search-block", {
-      left: "100%",
-      opacity: 0.5,
-    });
-    this.timeline.to(".search-block", {
-      left: 0,
-      opacity: 1,
-      duration: 0.33,
-    });
-    this.timeline.set(".search-block", {
-      borderBottom: "1px solid black",
-    });
-    // preload animation
-    this.timeline.progress(1).progress(0);
-  }
-
-  openSearch() {
-    this.timeline.play().then(() => {
-      document.getElementById("search-autocomplete")!.focus();
-    });
-  }
-
-  closeSearch() {
-    this.timeline.reverse().then(() => {
-      this.searchText = "";
-    });
+  toggleSearch() {
+    this.isSearchOpen = !this.isSearchOpen;
   }
 }
