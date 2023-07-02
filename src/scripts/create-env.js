826fc7fcfab2f2158686e16273cb682a23af60b1
Damianos <dgiankakis@citycollege.sheffield.eu>
2023-07-02 12:56:26 +0300

    Tries fix for netlify

--- src/scripts/create-env.js
+++ src/scripts/create-env.js
@@ -3,5 +3,5 @@ const fs = require('fs-extra');
 fs.outputFile('./src/environments/environment.ts', `export const environment = {
   production: true,
   WEATHER_API_KEY: "${process.env.WEATHER_API_KEY}",
-  WEATHER_API_BASEURL: "/api/"
+  WEATHER_API_BASEURL: "/api"
 };`);
